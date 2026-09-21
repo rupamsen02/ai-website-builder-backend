07ac1690dc6e05d91a0277f083ea727b7d2ae629
rupamsen02 <rupamsen195@gmail.com>
2026-09-21 18:13:42 +0530

    added

--- lib/auth.ts
+++ lib/auth.ts
@@ -31,8 +31,8 @@ export const auth = betterAuth({
             name: "auth_session",
             attributes: {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === "production",
-                sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+                secure: true,
+                sameSite: "none",
                 path: "/"
             }
         }
